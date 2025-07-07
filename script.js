b50ae9d187c67683ed83a983e417036d7140d966
KULDEEPDEV432 <kp4488773@gmail.com>
2025-07-07 21:15:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("ReadyBoot website loaded successfully.");
-  const downloadBtn = document.querySelector(".download-link");
-  if (downloadBtn) {
-    downloadBtn.addEventListener("click", () => {
-      console.log("Download button clicked.");
-      alert("✅ Your download will begin shortly!");
+  const btns = document.querySelectorAll('.download-btn');
+  btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+      alert("✅ Your download will start shortly!");
     });
-  }
+  });
 });
